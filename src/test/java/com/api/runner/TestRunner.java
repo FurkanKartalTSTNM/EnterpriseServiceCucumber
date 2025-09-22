fba7c7697fd63af372acc3366d8eb81b42c73b1f
FurkanKartalTSTNM <151616913+FurkanKartalTSTNM@users.noreply.github.com>
2025-09-22 10:46:58 +0300

    Update TestRunner.java

--- src/test/java/com/api/runner/TestRunner.java
+++ src/test/java/com/api/runner/TestRunner.java
@@ -8,10 +8,14 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/features",
-		glue = {"com.api.stepdefs", "com.api.utils"},
-		plugin = {"pretty", "html:target/cucumber-report"},
-		monochrome = true
+  features = "src/test/resources/features",
+  glue = {"com.api.stepdefs", "com.api.utils"},
+  plugin = {
+    "pretty",
+    "html:target/cucumber-report",
+    "json:target/cucumber.json",
+    "junit:target/surefire-reports/cucumber.xml"   // <-- JUnit XML buraya yazılır
+  },
+  monochrome = true
 )
-public class TestRunner {
-}
+public class TestRunner {}
